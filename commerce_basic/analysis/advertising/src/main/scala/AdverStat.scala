2034876891771da077127b77c7ccfeccccd6d32d
ceek <cpc_geek@163.com>
2020-12-30 00:26:52 +0800

    需求七：实时项目搭建

---
+++ commerce_basic/analysis/advertising/src/main/scala/AdverStat.scala
@@ -0,0 +1,11 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object AdverStat {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("AdverStat").setMaster("local[*]")
+      .set("spark.ui.port", "8999") //用于设置spark的ui端口，避免easyvpn端口4040冲突
+
+    val streamingContext = new StreamingContext(sparkConf, Seconds(5))
+  }
+}
