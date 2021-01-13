0b240a807ee668a020383551e9eccbb2a9add2eb
ceek <cpc_geek@163.com>
2021-01-13 23:42:37 +0800

    sparkstreaming-kafka中receive再探

--- Spark_Base/src/main/spark/org/pcchen/stream/SparkStreaming04_Kafka.scala
+++ Spark_Base/src/main/spark/org/pcchen/stream/SparkStreaming04_Kafka.scala
@@ -1,15 +1,12 @@
 package org.pcchen.stream
 
-import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
-import org.apache.spark.streaming.kafka.{HasOffsetRanges, KafkaUtils, OffsetRange}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka.KafkaUtils
 
 /**
   * sparkstreaming整合kafka
-  * receive模式
-  *
   * @author ceek
   * @create 2020-12-11 14:24
   **/
@@ -19,71 +16,31 @@ object SparkStreaming04_Kafka {
 
     val streamingContext: StreamingContext = new StreamingContext(sparkConf, Seconds(3));
 
-    val kafkaParam: Map[String, String] = Map[String, String](
-      ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> "org.apache.kafka.common.serialization.StringDeserializer",
-      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> "org.apache.kafka.common.serialization.StringDeserializer",
-      ConsumerConfig.GROUP_ID_CONFIG -> "sparkstreaming_demo",
-      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "10.10.32.60:9092"
-    )
 
     //启动后会反复消费kafka中的数据
     //从kafka中采集数据
     val createStream: ReceiverInputDStream[(String, String)] = KafkaUtils.createStream(
       streamingContext,
       "10.10.32.60:2181",
-      "sparkstreaming_demo", //分组id，唯一标识group_id
-      Map("sparkstreaming_test" -> 3) //topic->partition
+      "group_sparkstreaming_test", //分组id，唯一标识group_id
+      Map("topic_spark_receive8" -> 3) //topic->partition
     )
-
-    //直接打印数据
+    //直接答应数据
     createStream.print();
+
+    val dataStream: DStream[String] = createStream.map(_._2)
+    val keyOneStream: DStream[(String, Int)] = dataStream.transform {
+      rdd => {
+        rdd.map((_, 1))
+      }
+    }
+    keyOneStream.transform {
+      rdd => {
+        rdd.reduceByKey(_ + _)
+      }
+    }.print()
 
     streamingContext.start();
     streamingContext.awaitTermination();
-
-    /*val sparkConf = new SparkConf().setAppName("").setMaster("local[*]")
-
-    val ssc: StreamingContext = new StreamingContext(sparkConf, Seconds(2))
-
-    val createStream: ReceiverInputDStream[(String, String)] = KafkaUtils.createStream(
-      ssc,
-      "10.10.32.60:2181,10.10.32.61:2181,10.10.32.62:2181", //zookeeper的地址中间不能添见空格
-      "receive_group_test_1",
-      Map("sparkstreaming_test" -> 3)
-    )
-
-    /*var offsetRanges: Array[OffsetRange] = Array.empty[OffsetRange] // 定义一个集合，用来存放偏移量的范围
-    createStream.transform {
-      rdd => {
-        offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
-        rdd
-      }
-    }.foreachRDD {
-      rdd => {
-        for (o <- offsetRanges) {
-          println(s"${o.topic} ${o.partition} ${o.fromOffset} ${o.untilOffset}")
-        }
-      }
-    }
-
-    val transform: DStream[(String, Int)] = createStream.transform {
-      row => {
-        row.flatMap {
-          line => {
-            line._2.split(" ").map((_, 1))
-          }
-        }
-      }
-    }
-    val result = transform.reduceByKey(_ + _)*/
-
-    val result: DStream[(String, Int)] = createStream.flatMap(line => {
-      line._2.split(" ").map((_, 1))
-    }).reduceByKey(_ + _)
-    result.print()
-
-    ssc.start();
-
-    ssc.awaitTermination()*/
   }
 }
---
+++ Spark_Base/src/main/spark/org/pcchen/stream/SparkStreaming04_Kafka_Receive.scala
@@ -0,0 +1,14 @@
+package org.pcchen.stream
+
+/**
+  * sparkstreaming整合kafka
+  * 0.8中receive版本：0.10版本参考commerce_basic项目的advertising/streaming_kafka下
+  * offset保存在zookeeper中，会自动提交
+  * 服务重启后会从上次提交的offset处接着读取数据
+  *
+  * @author ceek
+  * @create 2021-01-13 21:27
+  **/
+object SparkStreaming04_Kafka_Receive {
+
+}
