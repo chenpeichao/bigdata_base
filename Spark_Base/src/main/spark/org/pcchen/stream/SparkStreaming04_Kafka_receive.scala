750fcbb408548db0a046690adc5b51f29bdb9138
ceek <cpc_geek@163.com>
2021-01-14 17:40:15 +0800

    sparkstreaming-kafka-0.8中receive自动提交offset和不同group消费数据

---
+++ Spark_Base/src/main/spark/org/pcchen/stream/SparkStreaming04_Kafka_receive.scala
@@ -0,0 +1,59 @@
+package org.pcchen.stream
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * streaming-kafka-0.8 receive模式(streaming-kafka-0.10参考commerce_basic/analysis/advertising/streaming_kafka)
+  *
+  * 使用高阶api，KafkaUtils.createStream;
+  * zookeeper来控制offset，会自动提交offset给zookeeper
+  *
+  * @author ceek
+  * @create 2021-01-14 10:57
+  **/
+object SparkStreaming04_Kafka_receive {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkStreaming04_Kafka_receive");
+
+    val ssc = new StreamingContext(sparkConf, Seconds(5));
+
+    val groupId = "group_sparkstreaming_test3";
+    //当添加多个消费者组时，会根据offset消费
+    val topicMap = Map("topic_spark_receive8" -> 3);
+    //此可以指定消费几个kafka的分区，数值小于等于topic的总分区数
+    val zookeeperInfo = "10.10.32.60:2181";
+
+    val dataStream: ReceiverInputDStream[(String, String)] = KafkaUtils.createStream(ssc, zookeeperInfo, groupId, topicMap)
+
+    val key2SumStream: DStream[(String, Int)] = dataStream.map(_._2).map((_, 1)).reduceByKey(_ + _);
+
+    val transform: DStream[(String, Int)] = key2SumStream.transform {
+      row => {
+        println("没消费");
+        row.foreach(x => println(x._2 + "->" + x._1))
+        row
+      }
+    }
+
+    transform.foreachRDD {
+      rdd => {
+        rdd.collect()
+      }
+    }
+
+    key2SumStream.foreachRDD {
+      rdd => {
+        println("-执行-");
+        rdd.collect().foreach(println)
+      }
+    }
+
+    //    key2SumStream.transform(x => {x.collect(); x})
+
+    ssc.start()
+    ssc.awaitTermination();
+  }
+}
