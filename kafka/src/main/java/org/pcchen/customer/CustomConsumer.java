f87344c82cd0c36d16ebc3b64a8d64ea9427d450
ceek <cpc_geek@163.com>
2023-01-02 12:47:31 +0800

    kafka自定义生产者和消费者测试

--- kafka/src/main/java/org/pcchen/customer/CustomConsumer.java
+++ kafka/src/main/java/org/pcchen/customer/CustomConsumer.java
@@ -15,37 +15,15 @@ import java.util.Properties;
  **/
 public class CustomConsumer {
     public static void main(String[] args) {
-
-//        Properties props = new Properties();
-//        props.put("bootstrap.servers", "10.10.32.59:9092");
-////        props.put("zookeeper.connect", "10.10.32.61:2181");
-//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-//        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-//
-//        props.put(ConsumerConfig.GROUP_ID_CONFIG, "1205");
-//
-//        //1.创建1个消费者
-//        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
-//
-//        consumer.subscribe(Arrays.asList("number"));
-//
-//        //2.调用poll
-//        while (true) {
-//            ConsumerRecords<String, String> records = consumer.poll(100);
-//            for (ConsumerRecord<String, String> record : records) {
-//                System.out.println("topic = " + record.topic() + " offset = " + record.offset() + " value = " + record.value());
-//            }
-//            consumer.commitAsync();
-//            consumer.commitSync();
-//        }
         Properties props = new Properties();
         // kafka servers
-        props.put("bootstrap.servers", "10.10.32.59:9092");
+        props.put("bootstrap.servers", "192.168.1.101:9092");
         // group
-        props.put("group.id", "test-consumer-group");
-        props.put("enable.auto.commit", "false");
+        props.put("group.id", "test-consumer-group");//消费者组，只要group.id相同，就属于同一个消费者组
         props.put("auto.commit.interval.ms", "1000");
+//        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put("enable.auto.commit", "false");//自动提交offset
+//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
--- kafka/src/main/java/org/pcchen/producer/CustomProducer.java
+++ kafka/src/main/java/org/pcchen/producer/CustomProducer.java
@@ -12,32 +12,14 @@ import java.util.Properties;
  * @create 2020-01-06 16:25
  **/
 public class CustomProducer {
-
-//    public static void main(String[] args) throws ExecutionException, InterruptedException {
-//        Properties props = new Properties();
-//        props.put("bootstrap.servers", "10.10.32.61:9092");//kafka集群，broker-list
-//        props.put("acks", "all");
-//        props.put("retries", 1);//重试次数
-//        props.put("batch.size", 16384);//批次大小
-//        props.put("linger.ms", 1);//等待时间
-//        props.put("buffer.memory", 33554432);//RecordAccumulator缓冲区大小
-//        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//
-//        Producer<String, String> producer = new KafkaProducer<String, String>(props);
-//        for (int i = 0; i < 100; i++) {
-//            producer.send(new ProducerRecord<String, String>("first", Integer.toString(i), Integer.toString(i)));
-//        }
-//        producer.close();
-//    }
-
     public static void main(String[] args) {
         Properties conf = new Properties();
-//        conf.put("--broker-list","192.168.0.30:9094");
-        conf.put("bootstrap.servers", "10.10.32.60:9093");
-//        conf.put("zookeeper","192.168.0.28:2181");
+        conf.put("bootstrap.servers", "192.168.1.101:9092");
         conf.put("acks", "all");
-//        conf.put("topic", args[0]);
+        conf.put("retries", 1);//重试次数
+        conf.put("batch.size", 16384);//批次大小
+        conf.put("linger.ms", 1);//等待时间
+        conf.put("buffer.memory", 33554432);//RecordAccumulator缓冲区大小
         conf.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         conf.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
